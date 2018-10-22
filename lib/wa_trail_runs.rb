d7612e6a436812d69cb6df3e8cdad720d68919d5
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-22 07:22:23 +0000

    Adds scraper file

--- lib/wa_trail_runs.rb
+++ lib/wa_trail_runs.rb
@@ -7,3 +7,5 @@ require_relative "./wa_trail_runs/version"
 require_relative "./wa_trail_runs/cli"
 
 require_relative "./wa_trail_runs/run"
+
+require_relative "./wa_trail_runs/scraper"
--- lib/wa_trail_runs/run.rb
+++ lib/wa_trail_runs/run.rb
@@ -10,43 +10,14 @@ class WaTrailRuns::Run
     runs = []
 
     runs << self.scrape_evergreen
-
+    runs << self.scrape_northwest
   end
 
-  def self.scrape_evergreen
-    race_hash = {}
-    evergreen_top = Nokogiri::HTML(open("http://www.evergreentrailruns.com/"))
-    evergreen_event = Nokogiri::HTML(open("http://www.evergreentrailruns.com/11-17-grand-ridge-trail-run"))
-    description = evergreen_event.css(".item_content p")[0].text
-    reg_link = evergreen_event.css("a[title='Register on Webscorer']").attribute("href").text
-    title = evergreen_top.css(".custom div")[1].children[1].to_s.split(" - ")[0].delete!("/<h4>/")
-    date = evergreen_top.css(".custom div")[1].children[1].to_s.split(" - ")[1].delete!("/<h4>/")
-    info_link = evergreen_top.css(".custom div")[1].children[3].children[0].attribute("href").text.prepend("www.evergreentrailruns.com")
-    race_hash[:description] = description
-    race_hash[:title] = title
-    race_hash[:date] = date
-    race_hash[:info_link] = info_link
-    race_hash[:reg_link] = reg_link
-    race_hash
-  end
 
-  def self.scrape_northwest
-    race_hash = {}
-    northwest = Nokogiri::HTML(open("http://nwtrailruns.com/events/"))
-    events = northwest.css(".event")
-    title = events.css(".the-title").first.text.strip
-    description = events.css(".the-content").first.children[1].text
-    info_link = events.css(".the-title").first.css("a").attribute("href").text
-    date = events.css(".the-date").first.text
-    reg_link = events.css(".the-links").first.children[3].attribute("href").text
-    race_hash[:title] = title
-    race_hash[:description] = description
-    race_hash[:date] = date
-    race_hash[:info_link] = info_link
-    race_hash[:reg_link] = reg_link
-    race_hash
-    binding.pry 
-  end
+
+
+  
+
 end
 
 
